9721ea3939ee5a06d2b409b04e864bf013362b16
lockinli <lockinli@zhaoxin.com>
2017-04-27 17:36:03 +0800

    liliangju porting STK3700 EFM32GG990F1024
    Signed-off-by: lockinli <lockinli@zhaoxin.com>

---
+++ PortingContest_STK3700_EFM32GG990F1024_liangjuli/projects/stm32f429_keil/Objects/los_api_list.d
@@ -0,0 +1,15 @@
+.\objects\los_api_list.o: ..\..\example\api\los_api_list.c
+.\objects\los_api_list.o: ..\..\kernel\include\los_list.h
+.\objects\los_api_list.o: ..\..\kernel\include\los_base.h
+.\objects\los_api_list.o: ..\..\platform\include\keil\los_builddef.h
+.\objects\los_api_list.o: ..\..\kernel\include\los_typedef.h
+.\objects\los_api_list.o: ..\..\platform\bsp\config\los_config.h
+.\objects\los_api_list.o: ..\..\kernel\include\los_printf.h
+.\objects\los_api_list.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\los_api_list.o: ..\..\kernel\include\los_list.h
+.\objects\los_api_list.o: ..\..\kernel\include\los_errno.h
+.\objects\los_api_list.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\los_api_list.o: ..\..\kernel\include\los_memory.h
+.\objects\los_api_list.o: ..\..\example\include\los_api_list.h
+.\objects\los_api_list.o: ..\..\example\include\los_demo_debug.h
+.\objects\los_api_list.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
