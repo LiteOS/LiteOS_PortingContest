47113374398ca6fb12907054a025ab79b7cee083
lockinli <lockinli@zhaoxin.com>
2017-05-04 11:34:22 +0800

    fix building warning
    Signed-off-by: lockinli <lockinli@zhaoxin.com>

--- PortingContest_STK3700_liliangju/projects/EFM32GG990F1024-KEIL/Objects/retargetio.d
+++
@@ -1,2 +0,0 @@
-.\objects\retargetio.o: D:\Keil_v5\ARM\PACK\Keil\EFM32GGxxx_DFP\2.3.0\Boards\SiliconLabs\common\drivers\retargetio.c
-.\objects\retargetio.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
--- PortingContest_STK3700_liliangju/projects/EFM32GG990F1024-KEIL/Objects/startup_efm32gg.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_efm32gg.o: RTE\Device\EFM32GG990F1024\startup_efm32gg.s
