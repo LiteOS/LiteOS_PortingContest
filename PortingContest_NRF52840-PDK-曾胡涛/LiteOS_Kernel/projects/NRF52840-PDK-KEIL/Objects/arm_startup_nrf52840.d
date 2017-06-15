b21d57d4dfd42f35ac9576418d2b5559144a5faf
taogal <976787288@qq.com>
2017-06-15 16:55:13 +0800

    the final code

---
+++ PortingContest_NRF52840-PDK-曾胡涛/LiteOS_Kernel/projects/NRF52840-PDK-KEIL/Objects/arm_startup_nrf52840.d
@@ -0,0 +1,1 @@
+.\objects\arm_startup_nrf52840.o: ..\..\platform\NRF52840-PDK\nRF52840_xxAA\arm_startup_nrf52840.s
---
+++ PortingContest_NRF52840-PDK-曾胡涛/LiteOS_Kernel/projects/STM32F411RE-NUCLEO-KEIL/Objects/los_bsp_key.d
@@ -0,0 +1,2 @@
+.\objects\los_bsp_key.o: ..\..\platform\STM32F411RE-NUCLEO\los_bsp_key.c
+.\objects\los_bsp_key.o: ..\..\platform\STM32F411RE-NUCLEO\los_bsp_key.h
