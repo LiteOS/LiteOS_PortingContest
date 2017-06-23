2ea7415171d82b71b5cf6abb21111a399509ec79
pengpeng0830 <xingzhipeng2@126.com>
2017-06-23 14:49:44 +0800

    update

---
+++ PortingContest_NUTINY-NUC472H_xingzhipeng/projects/NUTINY-NUC472H-KEIL/Objects/los_bsp_uart.d
@@ -0,0 +1,45 @@
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\los_bsp_uart.c
+.\objects\los_bsp_uart.o: C:\Program Files (x86)\Keil_v5_MDK\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\los_bsp_uart.o: C:\Program Files (x86)\Keil_v5_MDK\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\los_bsp_uart.o: C:\Program Files (x86)\Keil_v5_MDK\ARM\ARMCC\Bin\..\include\stdarg.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\los_bsp_uart.h
+.\objects\los_bsp_uart.o: C:\Program Files (x86)\Keil_v5_MDK\ARM\ARMCC\Bin\..\include\string.h
+.\objects\los_bsp_uart.o: ..\..\example\include\los_demo_debug.h
+.\objects\los_bsp_uart.o: ..\..\kernel\include\los_typedef.h
+.\objects\los_bsp_uart.o: ..\..\kernel\link\keil\los_builddef.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\NUC472_442.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\Include\core_cm4.h
+.\objects\los_bsp_uart.o: C:\Program Files (x86)\Keil_v5_MDK\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\Include\cmsis_compiler.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\Include\cmsis_armcc.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\system_NUC472_442.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\sys.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\clk.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\acmp.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\adc.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\eadc.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\cap.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\crypto.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\pdma.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\ebi.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\emac.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\fmc.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\gpio.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\i2c.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\pwm.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\epwm.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\rtc.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\sc.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\scuart.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\spi.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\timer.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\uart.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\usbd.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\wdt.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\wwdt.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\i2s.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\NUC472_442.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\can.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\sd.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\ps2.h
+.\objects\los_bsp_uart.o: ..\..\platform\NUTINY-NUC472H\Library\crc.h
---
+++ PortingContest_NUTINY-NUC472H_xingzhipeng/projects/NUTINY-NUC472H-KEIL/Objects/los_cmsis_main.d
@@ -0,0 +1,4 @@
+.\objects\los_cmsis_main.o: ..\..\example\api\los_cmsis_main.c
+.\objects\los_cmsis_main.o: ..\..\kernel\cmsis\cmsis_os.h
+.\objects\los_cmsis_main.o: C:\Program Files (x86)\Keil_v5_MDK\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\los_cmsis_main.o: C:\Program Files (x86)\Keil_v5_MDK\ARM\ARMCC\Bin\..\include\stddef.h
