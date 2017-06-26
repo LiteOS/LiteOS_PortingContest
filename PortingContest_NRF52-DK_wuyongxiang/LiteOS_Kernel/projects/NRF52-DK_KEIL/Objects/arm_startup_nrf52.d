d95120097bcd1fb9ced62156ffa174f6a7e09d62
wucool <wuyongxiangd@163.com>
2017-06-26 23:09:25 +0800

    delete object code

--- PortingContest_NRF52-DK_wuyongxiang/LiteOS_Kernel/projects/NRF52-DK_KEIL/Objects/arm_startup_nrf52.d
+++
@@ -1,1 +0,0 @@
-.\objects\arm_startup_nrf52.o: ..\..\platform\LOS_EXPAND_XXX\arm_startup_nrf52.s
--- PortingContest_NRF52-DK_wuyongxiang/LiteOS_Kernel/projects/NRF52-DK_KEIL/Objects/los_bsp_flash.d
+++
@@ -1,3 +0,0 @@
-.\objects\los_bsp_flash.o: ..\..\platform\NRF52-DK\los_bsp_flash.c
-.\objects\los_bsp_flash.o: ..\..\platform\NRF52-DK\los_bsp_flash.h
-.\objects\los_bsp_flash.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
