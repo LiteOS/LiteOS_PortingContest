2251fb54a2e92a15282386d137d45058e6039112
wucool <wuyongxiangd@163.com>
2017-06-27 14:16:07 +0800

    delete other project file

--- PortingContest_NRF52-DK_wuyongxiang/LiteOS_Kernel/projects/STM32F746ZG-NUCLEO-KEIL/Objects/los_bsp_led.d
+++
@@ -1,2 +0,0 @@
-.\objects\los_bsp_led.o: ..\..\platform\STM32F746ZG_NUCLEO\los_bsp_led.c
-.\objects\los_bsp_led.o: ..\..\platform\STM32F746ZG_NUCLEO\los_bsp_led.h
--- PortingContest_NRF52-DK_wuyongxiang/LiteOS_Kernel/projects/STM32F746ZG-NUCLEO-KEIL/Objects/los_startup_keil.d
+++
@@ -1,1 +0,0 @@
-.\objects\los_startup_keil.o: ..\..\platform\STM32F746ZG_NUCLEO\los_startup_keil.s
