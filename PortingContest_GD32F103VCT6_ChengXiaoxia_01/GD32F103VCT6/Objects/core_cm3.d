92e784d8489db8a7bbb847edfa0b856f021dc0fa
cheng1256788 <47661786@qq.com>
2017-06-30 15:10:54 +0800

    【移植活动第二期】GD32F103VCT6移植LiteOS 修改
    1、在GD32F103VCT6\platform\GD32F103VCT6\Library\Firmware\CMSIS中增加core_cmFunc.h，core_cmInstr.h，core_cmSimd.h来解除代码大小限制。2、删除多余的Listings和Objects文件夹

--- PortingContest_GD32F103VCT6_ChengXiaoxia_01/GD32F103VCT6/Objects/core_cm3.d
+++
@@ -1,2 +0,0 @@
-..\..\objects\core_cm3.o: ..\..\platform\GD32F103VCT6\Library\Firmware\CMSIS\core_cm3.c
-..\..\objects\core_cm3.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- PortingContest_GD32F103VCT6_ChengXiaoxia_01/GD32F103VCT6/Objects/startup_gd32f10x_hd.d
+++
@@ -1,1 +0,0 @@
-..\..\objects\startup_gd32f10x_hd.o: ..\..\platform\GD32F103VCT6\startup_gd32f10x_hd.s
